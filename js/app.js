83e3411944fd5e8edec80a1478e019a086f3486d
Cinzia Pulvirenti <cinzia2092@hotmail.it>
2023-02-06 20:57:19 +0100

    Feat aggiunto template literal

--- js/app.js
+++ js/app.js
@@ -1,41 +1,64 @@
-// const slideElement: []
+let slideElement = [
+    './img1.jpg',
+    './img2.jpg',
+    './img3.jpg',
+    './img4.jpg'
+]
+
 let slideAttiva = 0;
-let slideList = document.getElementsByClassName('slide');
-console.log(slideList);
+
+let elementJs = ''
+
+
+for (let i = 0; i < slideElement.length; i++) {
+    let activeClass = ''
+    if (i === 0) {
+        activeClass = 'active'
+    }
+    let img = `
+    <div class="slide ${activeClass}">
+        <img src="${slideElement[i]}" alt="">
+    </div>
+    `
+    elementJs += img
+}
+console.log('elemento', elementJs)
+document.getElementById('carosello').innerHTML += elementJs
+
 
 const rightArrowElement = document.getElementById("arrow-right-element");
 const leftArrowElement = document.getElementById("arrow-left-element");
 console.log(rightArrowElement, 'right-arrow');
+let slideList = document.getElementsByClassName('slide');
+console.log(slideList);
+
 
 rightArrowElement.addEventListener('click', function() {
     console.log(slideAttiva, 'slide attiva')
+    if (slideAttiva < slideList.length - 1) {
+        let slideCorrente = slideList[slideAttiva];
+        console.log(slideCorrente);
+        slideCorrente.classList.remove('active');
+        slideAttiva += 1;
 
-    let slideCorrente = slideList[slideAttiva];
-    console.log(slideCorrente);
-    slideCorrente.classList.remove('active');
-    slideAttiva += 1;
-
-    let prossimaslide = slideList[slideAttiva]
-    prossimaslide.classList.add('active')
-    console.log(prossimaslide)
-    
-    if (slideAttiva === slideList[4]) {
-        prossimaslide = slideList[1]
+        let prossimaslide = slideList[slideAttiva]
+        prossimaslide.classList.add('active')
+        console.log(prossimaslide)
     }
 })
 
 
 console.log('left-arrow', slideAttiva)
-leftArrowElement.addEventListener('click', function() {
-
-    let slideCorrente = slideList[slideAttiva];
-    console.log(slideCorrente);
-    slideCorrente.classList.remove('active');
-    slideAttiva += 1;
-
-    let prossimaslide = slideList[slideAttiva]
-    prossimaslide.classList.add('active')
-    console.log(prossimaslide)
+leftArrowElement.addEventListener('click', function () {
+    if (slideAttiva > 0) {
+        let slideCorrente = slideList[slideAttiva];
+        console.log(slideCorrente, 'ciaone');
+        slideCorrente.classList.remove('active');
+        slideAttiva -= 1;
+        let prossimaslide = slideList[slideAttiva]
+        prossimaslide.classList.add('active')
+        console.log(prossimaslide)
+    }
 })
 
 

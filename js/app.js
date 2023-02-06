aee8d7819bca1e2ef4c777ce6e7efce45d9dccd8
Cinzia Pulvirenti <cinzia2092@hotmail.it>
2023-02-06 16:38:11 +0100

    Feat struttura html + eventlistener js

---
+++ js/app.js
@@ -0,0 +1,24 @@
+// const slideElement: []
+let slideAttiva = 0;
+let slideList = document.getElementsByClassName('slide');
+console.log(slideList);
+
+const rightArrowElement = document.getElementById("arrow-right-element");
+const leftArrowElement = document.getElementById("arrow-left-element");
+console.log(rightArrowElement, 'right-arrow');
+
+rightArrowElement.addEventListener('click', function() {
+    console.log(slideAttiva, 'slide attiva')
+
+    let slideCorrente = slideList[slideAttiva];
+    console.log(slideCorrente);
+    slideCorrente.classList.remove('active');
+    slideAttiva += 1;
+
+    let prossimaslide = slideList[slideAttiva]
+    prossimaslide.classList.add('active')
+    console.log(prossimaslide)
+})
+
+
+
